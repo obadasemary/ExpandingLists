24c787945baa24bf99e89d7aa9c673ad59bcfb60
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-04 14:37:18 +0300

    Initial Commit

---
+++ ExpandingLists/ExpandingListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingListsApp.swift
+//  ExpandingLists
+//
+//  Created by Abdelrahman Mohamed on 4.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct ExpandingListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
